32c4b1d587f1f9e0cbd0384b2720416bbb794999
DESKTOP-52351T8\Admin <ee2ssam@gmail.com>
2024-11-19 16:56:33 +0900

    1. Enemy AI - Attack 구현 2. 플레이어 Health Bar 구현 3. 플레이어 Damage, Heal 플레시 효과 구현

--- Assets/FPS/Scripts/AI/EnemyController.cs
+++ Assets/FPS/Scripts/AI/EnemyController.cs
@@ -78,7 +78,9 @@ namespace Unity.FPS.AI
         public UnityAction OnDetectedTarget;
         public UnityAction OnLostTarget;
 
-        //
+        //Attack
+        public UnityAction OnAcctack;
+
         private float orientSpeed = 10f;
         public bool IsTargetInAttackRange => DetectionModule.IsTargetInAttackRange;
 
@@ -89,11 +91,17 @@ namespace Unity.FPS.AI
         public int currentWeaponIndex;
         private WeaponController currentWeapon;
         private WeaponController[] weapons;
+
+        //enemymanager
+        private EnemyManager enemyManager;
         #endregion
 
         private void Start()
         {
             //참조
+            enemyManager = GameObject.FindObjectOfType<EnemyManager>();
+            enemyManager.RegisterEnemy(this);                           //enemyManager에 등록
+
             Agent = GetComponent<NavMeshAgent>();
             actor = GetComponent<Actor>();
             selfColliders = GetComponentsInChildren<Collider>();
@@ -183,6 +191,9 @@ namespace Unity.FPS.AI
 
         private void OnDie()
         {
+            //enemyManager 리스트에 제거
+            enemyManager.RemoveEnemy(this);
+
             //폭발 효과
             GameObject effectGo = Instantiate(deathVfxPrefab, deathVfxSpawnPostion.position, Quaternion.identity);
             Destroy(effectGo, 5f);
@@ -279,7 +290,6 @@ namespace Unity.FPS.AI
 
             if(eyeRendererData.renderer)
             {
-                Debug.Log("================== OnDetected");
                 eyeColorMaterialPorpertyBlock.SetColor("_EmissionColor", attackEyeColor);
                 eyeRendererData.renderer.SetPropertyBlock(eyeColorMaterialPorpertyBlock,
                     eyeRendererData.metarialIndx);
@@ -291,8 +301,6 @@ namespace Unity.FPS.AI
         private void OnLost()
         {
             OnLostTarget?.Invoke();
-
-            Debug.Log("================== OnLost");
 
             if (eyeRendererData.renderer)
             {
@@ -353,10 +361,31 @@ namespace Unity.FPS.AI
             }
         }
 
-        //공격
-        public void TryAttack(Vector3 targetPosition)
+        //공격 - 공격성공, 실패
+        public bool TryAttack(Vector3 targetPosition)
         {
+            //무기 교체시 딜레이 시간동안 공격 불능
+            if (lastTimeWeaponSwapped + delayAfterWeaponSwap >= Time.time)
+            {
+                return false;
+            }
 
+            //무기 Shoot
+            bool didFire = GetCurrentWeapon().HandleShootInputs(false, true, false);
+
+            if (didFire && OnAcctack != null)
+            {
+                OnAcctack?.Invoke();
+
+                //발사를 한번 할때 마다 다음 무기로 교체
+                if (swapToNextWeapon == true && weapons.Length > 1)
+                {
+                    int nextWeaponIndex = (currentWeaponIndex + 1)% weapons.Length;
+                    SetCurrentWeapon(nextWeaponIndex);
+                }
+            }
+
+            return true;
         }
 
     }
